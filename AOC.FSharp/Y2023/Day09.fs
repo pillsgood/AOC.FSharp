22cc9efcfc3b8549315733985d438e1a761ae002
Pillsgood <arashm35@gamesoultech.com>
2024-11-21 10:28:10 +0800

    Add Day09 solution for 2023 in F#
    Implemented a new solution for Day09 of the 2023 Advent of Code in F#. This solution includes two parts with tests that extrapolate sequences from given inputs and submit the results.

---
+++ AOC.FSharp/Y2023/Day09.fs
@@ -0,0 +1,36 @@
+﻿namespace AOC.FSharp.Y2023
+
+open System.Text.RegularExpressions
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+open AOC.FSharp.Common
+
+[<TestFixture>]
+type Day09() =
+    inherit AocFixture()
+
+    let input = base.Input.Get<string[]>()
+
+    let sequences =
+        input
+        |> Seq.choose (function
+            | Matches (Regex(@"-?\d+")) groups -> Some(groups |> List.map (_.Value >> int64))
+            | _ -> None)
+        |> Seq.toList
+
+    let rec extrapolate xs =
+        let rec scan xs acc =
+            let ds = xs |> Seq.pairwise |> Seq.map (fun (a, b) -> b - a) |> Seq.toList
+            let i = acc + List.last xs
+
+            match ds |> Seq.sum with
+            | 0L -> i
+            | _ -> scan ds i
+
+        scan xs 0L
+
+    [<Test>]
+    member _.Part1() = sequences |> Seq.sumBy extrapolate |> base.Answer.Submit
+
+    [<Test>]
+    member _.Part2() = sequences |> List.map List.rev |> Seq.sumBy extrapolate |> base.Answer.Submit
