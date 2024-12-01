7cb2ccc01b1c6f3db5fc07d0e24f85896df7fe3e
Pillsgood <Arashm35@gmail.com>
2024-12-01 22:55:28 +0800

    Add initial solution for Y2024 Day01 challenge
    Include new Day01.fs file with part 1 and part 2 solutions for the year 2024 challenge. Update the project file to include Y2024 source files. The added tests validate each part before submitting the answers.

---
+++ AOC.FSharp/Y2024/Day01.fs
@@ -0,0 +1,36 @@
+﻿namespace AOC.FSharp.Y2024
+
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Collections
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+open AOC.FSharp.Common
+
+[<AocFixture>]
+module Day01 =
+
+    let input: string array = Input.fetch
+
+    let locations: (int * int) array =
+        input
+        |> Seq.choose (function
+            | MatchValue (Regex "(\d+)\s*(\d+)") [ left; right ] -> Some(int left, int right)
+            | _ -> None)
+        |> Seq.toArray
+
+    [<Test>]
+    let Part1 () =
+        let left = locations |> Seq.map fst |> Seq.sort
+        let right = locations |> Seq.map snd |> Seq.sort
+
+        Seq.zip left right |> Seq.sumBy (fun (l, r) -> abs (l - r)) |> Answer.submit
+
+
+    [<Test>]
+    let Part2 () =
+        let right = locations |> Seq.countBy snd |> Map
+
+        locations
+        |> Seq.map fst
+        |> Seq.sumBy (fun value -> value * (right |> Map.tryFind value |> Option.defaultValue 0))
+        |> Answer.submit
