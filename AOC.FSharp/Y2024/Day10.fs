bcd273f24f47739f2480c7c7d2e26839d493fae2
Pillsgood <arashm35@gamesoultech.com>
2024-12-10 14:38:38 +0800

    Add Day10 solution with pathfinding logic
    This commit introduces a new module for Day10 in the Y2024 directory. It includes a pathfinding algorithm to solve the puzzle using map and sequence operations. The solution is structured with tests to ensure correctness and uses parallel computation for efficiency.

---
+++ AOC.FSharp/Y2024/Day10.fs
@@ -0,0 +1,37 @@
+﻿namespace AOC.FSharp.Y2024
+
+open AOC.FSharp.Common
+open Microsoft.FSharp.Core
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+
+[<AocFixture>]
+module Day10 =
+    let input: Map<int2, int2 * int> =
+        Input.fetch<string array>
+        |> Array.rev
+        |> Array.mapi (fun j str -> str |> Seq.mapi (fun i c -> int2 (i, j), int (c - '0')))
+        |> Seq.concat
+        |> Seq.map (fun t -> fst t, t)
+        |> Map
+
+    let heads = input.Values |> Seq.filter (fun (_, v) -> v = 0) |> Seq.toArray
+
+    let rec pathFind (position, height) =
+        let search (position, height) =
+            int2.cardinalDirections
+            |> Array.choose (fun v -> input |> Map.tryFind (v + position))
+            |> Array.filter (snd >> (=) (height + 1))
+            |> Array.collect pathFind
+
+        (position, height) |> if height = 9 then Array.singleton else search
+
+    [<Test>]
+    let Part1 () =
+        heads
+        |> Array.Parallel.collect (pathFind >> Array.distinct)
+        |> Array.length
+        |> Answer.submit
+
+    [<Test>]
+    let Part2 () = heads |> Array.Parallel.collect pathFind |> Array.length |> Answer.submit
