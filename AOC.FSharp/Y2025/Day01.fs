90cede1377e6a01f0abd0c096e4a5bdc3c577420
Pillsgood <arashm35@gamesoultech.com>
2025-12-01 18:52:35 +0800

    Add Day01 solution for AoC 2025 and update project structure
    Introduce the initial solution for Day01 of AoC 2025, implementing core logic for both parts with NUnit tests. Updated the project file to include support for the Y2025 folder.

---
+++ AOC.FSharp/Y2025/Day01.fs
@@ -0,0 +1,34 @@
+﻿namespace AOC.FSharp.Y2025
+
+open System.Text.RegularExpressions
+open Microsoft.FSharp.Collections
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+
+[<AocFixture>]
+module Day01 =
+
+    let input =
+        Input.fetch
+        |> String.splitLines
+        |> Array.map (function
+            | MatchValue (Regex "L(\d+)") [ value ] -> -(int value)
+            | MatchValue (Regex "R(\d+)") [ value ] -> (int value)
+            | _ -> failwith "Invalid input")
+
+    let (%) = Math.modulo
+
+    [<Test>]
+    let Part1 () =
+        (50, input)
+        ||> Seq.scan (fun acc dial -> (acc + dial) % 100)
+        |> Seq.count ((=) 0)
+        |> Answer.submit
+
+    [<Test>]
+    let Part2 () =
+        input
+        |> Seq.collect (fun i -> Seq.replicate (abs i) (sign i))
+        |> Seq.scan (fun acc dial -> (acc + dial) % 100) 50
+        |> Seq.count ((=) 0)
+        |> Answer.submit
