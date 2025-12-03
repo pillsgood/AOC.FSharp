dd169ae729d9f0458602f702a7d25fca5ef83659
Pillsgood <arashm35@gamesoultech.com>
2025-12-03 14:16:40 +0800

    Add Day03 solution for AoC 2025 and extend string utilities
    Introduced the Day03 implementation with complete logic for both parts and NUnit tests. Enhanced `String.fs` by adding a `toList` utility function and removing the unused `asSpan`.

--- AOC.FSharp/Common/String.fs
+++ AOC.FSharp/Common/String.fs
@@ -2,8 +2,6 @@
 
 open System
 
-let inline asSpan (str: string) = str.AsSpan()
-
 let inline split (separator: string) (str: string) : string array =
     str.Split(separator, StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
 
@@ -15,6 +13,8 @@ let inline splitInto (count: int) (str: string) : string array =
     str |> Seq.splitInto count |> Seq.map String |> Seq.toArray
 
 let inline toArray (str: string) : char array = str.ToCharArray()
+
+let inline toList (str: string) : char list = str |> List.ofSeq
 
 let inline split2 (str: string) : string * string =
     str |> toArray |> Array.splitInto 2 |> (fun l -> String(l[0]), String(l[1]))
---
+++ AOC.FSharp/Y2025/Day03.fs
@@ -0,0 +1,24 @@
+﻿namespace AOC.FSharp.Y2025
+
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+
+[<AocFixture>]
+module Day03 =
+    let input = Input.fetch |> Array.map String.toArray
+
+    let max n bank =
+        let rec f (acc: string) (bank: char array) =
+            if acc.Length = n then
+                acc
+            else
+                let c = Array.max bank[.. ^((n - acc.Length) - 1)]
+                f $"{acc}{c}" (bank[((bank |> Array.findIndex ((=) c)) + 1) ..])
+
+        f "" bank |> int64
+
+    [<Test>]
+    let Part1 () = input |> Array.sumBy (max 2) |> Answer.submit
+
+    [<Test>]
+    let Part2 () = input |> Array.sumBy (max 12) |> Answer.submit
