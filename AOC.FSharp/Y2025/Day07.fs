5fa8cf900fd5fb911c218cdba4fd0b47c997cb00
Pillsgood <Arashm35@gmail.com>
2025-12-08 10:57:17 +0800

    Add Day07 solution for Y2025 with input parsing and test cases
    Implemented Parts 1 and 2 for Day07 in AOC.FSharp.Y2025. Includes input parsing logic with splitters and start position processing, as well as recursive computations for both parts. Added test cases to validate the solution.

---
+++ AOC.FSharp/Y2025/Day07.fs
@@ -0,0 +1,60 @@
+﻿namespace AOC.FSharp.Y2025
+
+open AOC.FSharp.Common
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+
+[<AocFixture>]
+module Day07 =
+    let input =
+        Input.fetch<string array>
+        |> fun src ->
+            {| Splitters =
+                src
+                |> Array.choose2d (fun i j c -> if c = '^' then Some(int2 (i, j)) else None)
+                |> Set.ofArray
+               Start =
+                src
+                |> Array.choose2d (fun i j c -> if c = 'S' then Some(int2 (i, j)) else None)
+                |> Array.head
+               Length = src.Length |}
+
+    [<Test>]
+    let Part1 () =
+        let rec scan (t: int) (acc: int list) (j: int) =
+            if j >= input.Length then
+                t
+            else
+                let acc', t' =
+                    acc
+                    |> List.fold
+                        (fun (next, counts) i ->
+                            if input.Splitters.Contains(int2 (i, j)) then
+                                (i - 1) :: (i + 1) :: next, counts + 1
+                            else
+                                i :: next, counts)
+                        ([], 0)
+
+                scan (t + t') (List.distinct acc') (j + 2)
+
+        scan 0 [ input.Start.x ] input.Start.y |> Answer.submit
+
+    [<Test>]
+    let Part2 () =
+        let rec scan (acc: (int * int64) list) (j: int) =
+            if j >= input.Length then
+                acc |> List.sumBy snd
+            else
+                let acc' =
+                    acc
+                    |> List.collect (fun (i, t) ->
+                        if input.Splitters.Contains(int2 (i, j)) then
+                            [ i - 1, t; i + 1, t ]
+                        else
+                            [ i, t ])
+                    |> List.groupBy fst
+                    |> List.map (fun (i, t) -> i, t |> List.sumBy snd)
+
+                scan acc' (j + 2)
+
+        scan [ input.Start.x, 1L ] input.Start.y |> Answer.submit
