255b3a0fc0e1456cb24f83f8956450d0954208f2
Pillsgood <arashm35@gamesoultech.com>
2025-12-11 14:38:58 +0800

    Add Day11 solution for AoC 2025 with path counting logic and tests

---
+++ AOC.FSharp/Y2025/Day11.fs
@@ -0,0 +1,43 @@
+﻿namespace AOC.FSharp.Y2025
+
+open System.Text.RegularExpressions
+open AOC.FSharp.Common
+open NUnit.Framework
+open Pillsgood.AdventOfCode
+
+[<AocFixture>]
+module Day11 =
+    type Connection = { src: string; dsts: string list }
+
+    let input: Map<string, Connection> =
+        Input.fetch
+        |> Array.choose (function
+            | Match (Regex @"^(\w{3})\:(?:\s?(\w{3})\s??)+$") [ src; dsts ] ->
+                Some { src = src.Value; dsts = dsts.Captures |> Seq.map _.Value |> List.ofSeq }
+            | _ -> None)
+        |> Array.map (fun c -> c.src, c)
+        |> Map.ofArray
+
+    let countPaths (dst: string) req src =
+        memoizeRec
+        <| fun f' (req, current) ->
+            let req' = req |> List.filter ((<>) current)
+
+            let search node : int64 =
+                input
+                |> Map.tryFind node
+                |> Option.map _.dsts
+                |> Option.defaultValue []
+                |> List.sumBy (fun dst' -> f' (req', dst'))
+
+            if current = dst then
+                if req.IsEmpty then 1L else 0L
+            else
+                search current
+        |> fun f -> f (req, src)
+
+    [<Test>]
+    let Part1 () = "you" |> countPaths "out" [] |> Answer.submit
+
+    [<Test>]
+    let Part2 () = "svr" |> countPaths "out" [ "fft"; "dac" ] |> Answer.submit
